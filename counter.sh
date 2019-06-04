6cfb5025c60c65e85e9010b5771f612b3e1b9e98
Christopher Amurao <camurao@csatf.org>
2019-06-04 13:38:54 -0700

    added some lines

--- counter.sh
+++ counter.sh
@@ -12,19 +12,19 @@ echo $check_items
 
 # find the amount of these things
 p_one=$(grep -o "P04025001TB" $1 | wc -l)
-echo $p_one
+echo "P04025001TB: $p_one"
 
 p_two=$(grep -o "P040" $1 | wc -l)
-echo $p_two
+echo "P040 $p_two"
 
 p_three=$(grep -o "P041" $1 | wc -l)
-echo $p_three
+echo "P041 $p_three"
 
 p_four=$(grep -o "P042" $1 | wc -l)
-echo $p_four
+echo "P042 $p_four"
 
 
 # Add the number of p040, p041, and p042 records 
 # subtract the number of check items from this count and divide by 8, the result should match the number of checks in the batch
-
-
+added=$(($p_two + $p_three + $p_four))
+echo "Total number of records: $added" 
