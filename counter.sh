552ae61ae0daf64895d96ede96e9a8c4fe5a59ed
chrisamurao <freshpotatoes@gmail.com>
2019-06-19 06:18:36 -0700

    mostly done, almost working for counter

--- counter.sh
+++ counter.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 
 #read the amount of checks in the batch
+#maybe add web parser for these steps -> beautiful soup-ish or roll own
 echo "Enter amount of checks in the batch:"
 read $checks
 echo $checks
@@ -15,16 +16,29 @@ p_one=$(grep -o "P04025001TB" $1 | wc -l)
 echo "P04025001TB: $p_one"
 
 p_two=$(grep -o "P040" $1 | wc -l)
-echo "P040 $p_two"
+echo "P040: $p_two"
 
 p_three=$(grep -o "P041" $1 | wc -l)
-echo "P041 $p_three"
+echo "P041: $p_three"
 
 p_four=$(grep -o "P042" $1 | wc -l)
-echo "P042 $p_four"
+echo "P042: $p_four"
 
 
 # Add the number of p040, p041, and p042 records 
-# subtract the number of check items from this count and divide by 8, the result should match the number of checks in the batch
 added=$(($p_two + $p_three + $p_four))
 echo "Total number of records: $added" 
+
+# subtract the number of check items from this count and divide by 8, the result should match the number of checks in the batch
+
+#minus sign throwing error??
+# subtracted=python -c "print $added + $check_items"
+# echo $[$added-$check_items]
+# subtracted=$(python << END print($added - $check_items) END)
+echo "Subtracted: $subtracted"
+
+# if [[ $subtracted / 8 == $check_items ]]
+if [[ 1 = 1 ]]
+then
+  echo "OK"
+fi
